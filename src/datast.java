f7a00b71845fd1312d89a186f139584de2b10fbc
Japaranha <kazucalado@gmail.com>
2020-10-20 23:59:50 -0300

    tomara que de certo

---
+++ src/datast.java
@@ -0,0 +1,26 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class datast {
+	public static void main(String[] args) {
+		SimpleDateFormat dataf1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat dataf2 = new SimpleDateFormat("HH:mm:ss");
+		Date day = new Date();
+		
+		
+		Timer timer = new Timer();
+		final long SEGUNDOS = (1000);
+		TimerTask tarefa = new TimerTask() {
+
+			@Override
+			public void run() {
+				System.out.println(dataf2.format(new Date()));
+				
+			}
+			
+		};
+		timer.schedule(tarefa, 0,SEGUNDOS);
+	}
+}
